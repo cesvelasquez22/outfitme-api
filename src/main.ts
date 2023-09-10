c21978169dd64685778b6fc6a4258c9ba5547ef1
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2023-09-10 13:24:33 -0600

    chore: remove unused

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('NestApplication');
+  app.setGlobalPrefix('api');
   await app.listen(3000);
+  // logger.log(`App running on port ${process.env.PORT}`);
+  logger.log(`App running on port 3000`);
 }
 bootstrap();
