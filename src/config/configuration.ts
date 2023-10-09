84963a3a76ba8043775fae24e9d9ad06a7c3cbef
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2023-10-08 18:01:53 -0600

    refactor(config): use database uri

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,11 +1,8 @@
 export default () => ({
   production: process.env.NODE_ENV === 'production',
+  stage: process.env.STAGE || 'development',
   port: parseInt(process.env.PORT, 10) || 3000,
   database: {
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 10) || 3306,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    url: process.env.DB_URL,
   },
 });
